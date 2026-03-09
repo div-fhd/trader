94bed0f22aa195c3475e83fd6b91e29e71054037
serviess <fack21gasa@gmail.com>
2026-03-09 15:12:27 +0200

    refactor: simplify heartbeat function by removing setInterval and async/await

--- jobs/heartbeat.job.js
+++ jobs/heartbeat.job.js
@@ -1,11 +1,10 @@
 const telegram = require("../services/telegram.service");
 
 function startHeartbeat() {
-
-  setInterval(async () => {
+ 
     try {
 
-      await telegram.sendMessage(
+       telegram.sendMessage(
         `🤖 Bot Alive\n⏰ ${new Date().toLocaleString()}`
       );
 
@@ -16,8 +15,7 @@ function startHeartbeat() {
       console.log("Heartbeat error:", err.message);
 
     }
-  }, 1800000); // كل 30 دقيقة
-
+ 
 }
 
 module.exports = {
